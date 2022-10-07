a2b42393f591f136e6b6e2b1aec86d159b6706f5
Juandi <jmarmor2111@g.educaand.es>
2022-10-07 09:42:48 +0200

    Ejercicios 2 y 8 secuenciales compleatados

--- src/main/java/tema2_ejercicios_secuenciales/Ej08.java
+++ src/main/java/tema2_ejercicios_secuenciales/Ej08.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tema2_ejercicios_secuenciales;
 
-/**
- *
- * @author juand
- */
+import java.util.Scanner;
+
+
 public class Ej08 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+         Scanner entrada = new Scanner (System.in);
+        double presupuestoAnual;
+        
+        
+        System.out.println("Da valor al presupuesto anual");
+        
+        presupuestoAnual= entrada.nextDouble();
+        
+        double cuarentaporciento= presupuestoAnual*0.4;
+        
+        System.out.println("El presupuesto de ginecología es: "+(cuarentaporciento)+" €");
+        
+        double treintaPorciento= presupuestoAnual*0.3;
+        
+        System.out.println("El presupuesto de traumatología es: "+(treintaPorciento)+" €");
+        System.out.println("El presupuesto de pediatría es: "+(treintaPorciento)+" €");
     }
     
 }
