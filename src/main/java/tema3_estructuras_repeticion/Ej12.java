bc4807586c863a0fa86922efecde876e635b4bb0
juandi <jmarmor2111@g.educaand.es>
2022-10-12 02:57:02 +0200

    Ejercicios tema 3 terminados

---
+++ src/main/java/tema3_estructuras_repeticion/Ej12.java
@@ -0,0 +1,23 @@
+
+package tema3_estructuras_repeticion;
+
+
+public class Ej12 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        int codNumerico=97;
+        
+        for (int i = 0; i <= 25; i++) {
+            
+            System.out.print(codNumerico);
+            System.out.print("-");
+            codNumerico++;
+        }
+        
+        
+    }
+    
+}
---
+++ src/main/java/tema3_estructuras_repeticion/Ej13.java
@@ -0,0 +1,23 @@
+
+package tema3_estructuras_repeticion;
+
+
+public class Ej13 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        int codNumerico=65;
+        
+        do {      
+            
+            System.out.print(codNumerico);
+            System.out.print("-");
+            codNumerico++;
+            
+            
+        } while (codNumerico<91);
+    }
+    
+}
