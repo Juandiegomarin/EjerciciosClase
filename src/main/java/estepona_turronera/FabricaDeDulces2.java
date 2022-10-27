0feb6d7a37ce18ff4aa980d6a5fea6db8579e046
juandi <jmarmor2111@g.educaand.es>
2022-10-27 15:23:02 +0200

    Finalizacion tarea Fabrica de dulces 2

--- src/main/java/estepona_turronera/FabricaDeDulces2.java
+++ src/main/java/estepona_turronera/FabricaDeDulces2.java
@@ -1,66 +1,188 @@
 package estepona_turronera;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class FabricaDeDulces2 {
 
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
+        
+        //Todas las variables necesarias para el programa
         String opcion;
-        String codigo;
+        String codigo = "";
 
+        final double COSTE_MINIMO_MATERIA_PRIMA = 0.1;
+        final double COSTE_MAXIMO_MATERIA_PRIMA = 1.0;
+        
+        final double COSTE_MINIMO_MANO_OBRA = 0.5;
+        final double COSTE_MAXIMO_MANO_OBRA = 0.9;
+
+        final double PORCENTAJE_VENTA_UNITARIA_M1_M2_P1 = 0.5;
+        final double PORCENTAJE_VENTA_RESTO_ARTICULOS = 0.65;
+        
+        final int PRECIO_BENEFICIO=2500;
+        
+        double unidadesParaObtenerBeneficio;
+
+        double precioMateriaPrima;
+        double precioManoObra;
+        double costeProduccion;
+        double costeVentaUnitaria;
+
+        String menu = """
+                       Menu de opciones
+                        ----------------
+                        Calcular viabilidad de productos
+                        Terminar
+                        ----------------
+                       """;
         do {
+            //Bucle principal que contiene un switch con las dos opciones que se pide
 
-            System.out.println("Menu de opciones");
-            System.out.println("----------------");
-            System.out.println("Calcular viabilidad de productos");
-            System.out.println("Terminar");
-            System.out.println("----------------");
+            JOptionPane.showMessageDialog(null, menu);
 
-            System.out.println("");
-            System.out.println("");
+           opcion= JOptionPane.showInputDialog(null,
+                    "Para calcular la viabilidad escriba calcular "
+                    + ", para terminar escriba salir");
 
-            System.out.println("Para calcular la viabilidad escriba calcular");
-            System.out.println("Para terminar escriba salir");
-            opcion = entrada.nextLine();
+            //Transformamos la variable a minuscula para facilitar los case del switch principal
             opcion = opcion.toLowerCase();
 
             switch (opcion) {
                 case "calcular":
-
-                    System.out.println("Ahora introduce el codigo del dulce deseado");
-                                           
-                        
-                  
+                    //Primer caso primer switch
+                    
+                    
+                    JOptionPane.showMessageDialog(null,
+                            "Ahora introduce el codigo del dulce deseado");
+                    //Pedimos datos por pantalla y lo introducimos
+                    
+                    //bucle interno que se repite si la intruduccion del codigo es errónea
                     do {
-                        codigo = entrada.nextLine();
-                        if (!(codigo.equals("M1")
-                                || codigo.equals("M2")
-                                || codigo.equals("T1")
-                                || codigo.equals("T2")
-                                || codigo.equals("P1"))) {
-                            System.out.println("Codigo incorrecto, vuelva a introducir");
+                      codigo =  JOptionPane.showInputDialog(null,
+                                "Los códigos disponibles son M1,M2,T1,T2,P1. "
+                                + "Para terminar el programa escriba salir");
+
+                        //switch intermedio que tambien tiene dos opciones o codigo o salir
+                                
+                        switch (codigo) {
+                            case "M1","M2","T1","T2","P1":
+                                //Primer caso segundo switch
+                                JOptionPane.showMessageDialog(null,
+                                        "El codigo es: " + codigo);
+                                
+                                //bucle que repite la pedida de datos si no se encuentra entre los
+                                // rangos limitados
+                                do {
+                                    precioMateriaPrima = Double.parseDouble
+                                    (JOptionPane.showInputDialog
+                                    ("Ahora introduce el precio de la materia prima"
+                                            + ", debe estar entre 0.1 y 1"));
+                                    
+                                    //Condicion que salta mensaje si no se cumple los requisitos de
+                                    //entrada
+                                    if (precioMateriaPrima < COSTE_MINIMO_MATERIA_PRIMA
+                                            || precioMateriaPrima > COSTE_MAXIMO_MATERIA_PRIMA) {
+                                        JOptionPane.showMessageDialog(null,
+                                                "Precio incorrecto, repita");
+                                    }
+
+                                } while (precioMateriaPrima < COSTE_MINIMO_MATERIA_PRIMA
+                                        || precioMateriaPrima > COSTE_MAXIMO_MATERIA_PRIMA);
+                                
+                                //bucle que repite la pedida de datos si no se encuentra entre los
+                                // rangos limitados
+                                
+                                do {
+                                    precioManoObra = Double.parseDouble
+                                    (JOptionPane.showInputDialog
+                                    ("Ahora introduce el precio de la mano de obra"
+                                            + ", debe estar entre 0.5 y 0.9"));
+                                    
+                                    //Condicion que salta mensaje si no se cumple los requisitos de
+                                    //entrada
+                                    if (precioManoObra < COSTE_MINIMO_MANO_OBRA
+                                            || precioManoObra > COSTE_MAXIMO_MANO_OBRA) {
+                                        JOptionPane.showMessageDialog(null,
+                                                "Precio incorrecto, repita");
+                                    }
+
+                                } while (precioManoObra < COSTE_MINIMO_MANO_OBRA
+                                        || precioManoObra > COSTE_MAXIMO_MANO_OBRA);
+                                
+                                //Inicializacion de variable para usarla
+                                costeProduccion = precioManoObra + precioMateriaPrima;
+
+                                JOptionPane.showMessageDialog(null,
+                                        "El coste de produccion del codigo " + codigo+" es: "
+                                        + String.format("%.2f", costeProduccion) + "€");
+                                        //String format para delimitar los decimales a 2
+                                
+                                //Ultimo switch para diferenciar el precio de venta unitaria
+                                //entre codigos
+                                switch (codigo) {
+                                    case "M1","M2","P1":
+                                        //primer caso tercer switch
+                                        costeVentaUnitaria = costeProduccion + costeProduccion
+                                                * PORCENTAJE_VENTA_UNITARIA_M1_M2_P1;
+
+                                        break;
+                                    default:
+                                        //equivalente al segundo caso tercer switch
+                                        //ya qur ya filtramos la entrada de datos anteriormente
+                                        costeVentaUnitaria = costeProduccion + costeProduccion
+                                                * PORCENTAJE_VENTA_RESTO_ARTICULOS;
+                                }
+                                
+                                JOptionPane.showMessageDialog(null,
+                                        "El coste de venta unitaria del codigo " + codigo+" es: "
+                                        + String.format("%.2f",costeVentaUnitaria ) + "€");
+                                
+                                //Inicializacion de variable que nos va a servir para calcular las
+                                //unidades necesarias para obterner beneficio
+                                unidadesParaObtenerBeneficio=Math.ceil(PRECIO_BENEFICIO/
+                                        (costeVentaUnitaria-costeProduccion));
+                                
+                                JOptionPane.showMessageDialog(null, 
+                                        "El numero de unidades para el beneficio son:"
+                                                + " "+(int)unidadesParaObtenerBeneficio+ " unidades");
+                                break;
+                            case "salir":
+                                //Segundo caso segundo switch
+                                JOptionPane.showMessageDialog(null,
+                                        "Programa finalizado");
+                                
+                               
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null,
+                                        "Codigo inexsistente, repita");
+
                         }
 
-                    } while (!(codigo.equals("M1")
+                    } while (!codigo.equals("salir") && !(codigo.equals("M1")
                             || codigo.equals("M2")
                             || codigo.equals("T1")
                             || codigo.equals("T2")
                             || codigo.equals("P1")));
-                    
+
                     break;
 
                 case "salir":
-                    System.out.println("Programa finalizado");
+                    //Segundo caso primer switch
+                     JOptionPane.showMessageDialog(null,
+                                        "Programa finalizado");
                     break;
 
                 default:
-                    System.out.println("Error, vuelva a introducir");
-                    System.out.println("");
+                     JOptionPane.showMessageDialog(null,
+                                        "Error, vuelva a intentarlo");
             }
 
-        } while (!opcion.equals("salir"));
-    }
+        } while (!opcion.equals("salir")&&!codigo.equalsIgnoreCase("salir"));
+    }   //Condicion para que se repita en el caso de que la variable opcion
+    //ni la variable codigo sea salir
 
 }
