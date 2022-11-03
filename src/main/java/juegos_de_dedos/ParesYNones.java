7173b8becdbb02fcba0222c05fb8db3e8c3c4f31
juandi <jmarmor2111@g.educaand.es>
2022-11-03 15:50:11 +0100

    Juegos de manos , pares y nones y morra terminados

---
+++ src/main/java/juegos_de_dedos/ParesYNones.java
@@ -0,0 +1,117 @@
+package juegos_de_dedos;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ParesYNones {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        Random rd = new Random();
+
+        int numeroMaquina;
+        int numeroUsuario = 0;
+        int suma = 0;
+        boolean seguir;
+        String opcion = "";
+
+        System.out.println("Bienvenido al juego de pares o nones");
+        System.out.println("En este programa vas a jugar contra la maquina");
+        System.out.println("Para ello elige primero pares o nones");
+
+        //Bucle que se repite si introducimos algo que no sea ni pares ni nones
+        do {
+
+            opcion = entrada.nextLine();
+            opcion = opcion.toLowerCase();
+            //Pasamos a minúsculas, lo utilizaremos para entrar a los case del 
+            //Switch
+            
+            //Condición que saca mensaje si la opcion no es ni pares ni nones
+            if (!opcion.equalsIgnoreCase("pares") && !opcion.equalsIgnoreCase("nones")) {
+                System.out.println("Vuelve a introducir");
+            }
+
+        } while ((!opcion.equalsIgnoreCase("pares") && !opcion.equalsIgnoreCase("nones")));
+        
+        
+        
+        System.out.println("Has elegido " + opcion);
+        System.out.println("Elige un numero del 0 al 10");
+        //Pedimos al usuario un número del 0 al 10
+        
+        
+        
+        //Filtramos tanto que no se introduzcan valores numéricos tanto como
+        //que se introduzcan numero entre 1 y 10 con doble bucle do while y try
+        //catch
+        do {
+            seguir = true;
+            do {
+
+                try {
+                    numeroUsuario = entrada.nextInt();
+                    seguir = false;
+
+                } catch (InputMismatchException ime) {
+                    System.out.println("Valor no numerico, repita");
+                }
+                entrada.nextLine();
+            } while (seguir);
+
+            if (numeroUsuario < 0 || numeroUsuario > 10) {
+                System.out.println("Numero fuera de rango, repita");
+            }
+        } while (numeroUsuario < 0 || numeroUsuario > 10);
+        
+        
+        
+        
+        
+        
+        //Generamos un numero random entre 0 y 10 que será el de la máquina
+        numeroMaquina = rd.nextInt(11);
+        
+        //Inicializamos la variable con el valor de la suma de los dos números
+        //el nuestro ya filtrado y el generado de la máquina
+        suma = numeroMaquina + numeroUsuario;
+        
+        
+        //Mensajes para comprobar los valores de cada número previamente
+        System.out.println("Tu numero es " + numeroUsuario);
+        System.out.println("El numero de la máquina es " + numeroMaquina);
+        
+        //Switch según la opción elegida previamente
+        //La suma habra dado un valor par o impar y según lo que hayamos elegido, pares o 
+        //nones ganaremos o perderemos
+        switch (opcion) {
+            case "pares":
+                
+                //Si hemos elegido pares y el resto de dividir entre 2 es 0
+                //Habremos ganado al contrario habremos perdido
+                System.out.println("La suma es " + suma);
+                if (suma % 2 == 0) {
+                    System.out.println("Has ganado");
+                } else {
+                    System.out.println("Has perdido");
+                }
+
+                break;
+            case "nones":
+                //Si hemos elegido nones y el resto de dividir entre 2 es 0
+                //Habremos perdido al contrario habremos ganado
+                System.out.println("La suma es " + suma);
+                if (suma % 2 == 0) {
+                    System.out.println("Has perdidio");
+                } else {
+                    System.out.println("Has ganado");
+                }
+                break;
+
+        }
+
+    }
+
+}
