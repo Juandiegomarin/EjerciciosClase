03a4a7259a91215a0f302557b97b82495ea29a81
juandi <jmarmor2111@g.educaand.es>
2022-11-03 14:25:15 +0100

    Finalizacion refactorizacion fabrica de dulces 3.0 y añadido de comentarios explicativos sobre el programa

--- src/main/java/estepona_turronera/FabricaDeDulces3.java
+++ src/main/java/estepona_turronera/FabricaDeDulces3.java
@@ -64,19 +64,41 @@ public class FabricaDeDulces3 {
                     
                     switch (codigoDulce) {
                         case "M1","M2","T1","T2","P1":
-
+                            
+                            //En esta variable se almacena el valor devuelto por
+                            //el método filtrarCosteMateriaPrima
                             precioMateriaPrima = filtrarCosteMateriaPrima();
-
+                            
+                            
+                            //En esta variable se almacena el valor devuelto por
+                            //el método filtrarCosteManoObra
                             precioManoObra = filtrarCosteManoObra();
+                            
+                            //En esta variable se almacena el valor devuelto por
+                            //el método calcularCosteProduccion
+                            costeProduccion = calcularCosteProduccion(
+                                    precioMateriaPrima,
+                                    precioManoObra,
+                                    codigoDulce);
 
-                            costeProduccion = calcularCosteProduccion(precioMateriaPrima,
-                                    precioManoObra, codigoDulce);
-
-                            costeVentaUnitaria = calcularCosteVentaUnitaria(costeProduccion, codigoDulce);
-
-                            unidadesParaObtenerBeneficio = calcularUnidadesParaObtenerBeneficios(costeProduccion, costeVentaUnitaria, PRECIO_BENEFICIO);
+                            
+                            //En esta variable se almacena el valor devuelto por
+                            //el método calcularCosteVentaUnitaria
+                            costeVentaUnitaria = calcularCosteVentaUnitaria(
+                                    costeProduccion,
+                                    codigoDulce);
+                            
+                            
+                            //En esta variable se almacena el valor devuelto por
+                            //el método calcularUnidadesParaObtenerBeneficios
+                            unidadesParaObtenerBeneficio =
+                                         calcularUnidadesParaObtenerBeneficios(
+                                            costeProduccion,
+                                            costeVentaUnitaria,
+                                            PRECIO_BENEFICIO);
                             break;
                         case "salir":
+                            //Si codigo es salir salta este mensaje
                             JOptionPane.showMessageDialog(null,
                                     "Finalizando programa");
                             break;
@@ -86,7 +108,7 @@ public class FabricaDeDulces3 {
                     break;
 
                 case "salir":
-
+                    //Si opcion es salir salta este mensaje
                     JOptionPane.showMessageDialog(null,
                             "Programa finalizado");
 
@@ -104,12 +126,16 @@ public class FabricaDeDulces3 {
                 && !codigoDulce.equalsIgnoreCase("salir"));
 
     }
+    
+    
+    
 
     //Este método muestra el menu de opciones a la vez que te pide la opcion
     // al escribir la opcion esta se devuelve mediante el return
     private static String mostrarMenuPedirOpcion() {
 
-        //Variable menu que guarda un tectblock sobre el menú principal del código
+        //Variable menu que guarda un tectblock sobre el menú principal
+        //del código
         String menu = """
                        Menú de opciones
                         ----------------
@@ -122,6 +148,10 @@ public class FabricaDeDulces3 {
                 + "Calcular para seguir, salir o cancelar para terminar");
 
     }
+    
+    
+    
+    
 
     //Este método te muestra los códigos de dulces disponibles a la vez que te
     //pide que escribas el código que le quieras a hacer los calculos
@@ -143,8 +173,12 @@ public class FabricaDeDulces3 {
     
     
     
+    
+    
     //Este método filtra el código para que sea salir o uno de los códigos 
     //disponibles
+    //Devuelve un valor String que será guardado posteriormente en variables del
+    //método mostrarPrograma
     private static String filtrarCodigoDulce() {
         //Variable temporal para guardar el código
         String codigo = "";
@@ -192,10 +226,13 @@ public class FabricaDeDulces3 {
     
     
     
+    
     //Este método te muestra entre que precio debe estar el coste de la materia
-    //prima, devuelve el velor que escribas por teclado
+    //prima, devuelve el valor que escribas por teclado
     //Este valor se guardara en una variable temporal en el método
     //filtrarCosteMateriaPrima
+    //Devuelve un valor String que será guardado posteriormente en variables del
+    //método filtrarCosteMateriaPrima
     private static String leerCosteMateriaPrima() {
 
         return JOptionPane.showInputDialog("Introduce el precio de la materia prima"
@@ -206,7 +243,13 @@ public class FabricaDeDulces3 {
     
     
     
-
+    //Este metodo lo usamos para filtrar la entrada de datos y se encuentre
+    //entre los parámetros indicados
+    //Cun bucle que llama al método leerCosteMateriaPrima cada vez que nos 
+    //equivoquemos al introducir los datos, mas explicado en el interior del
+    //método
+    //Devuelve un valor double que será guardado posteriormente en variables del
+    //método mostrarPrograma
     private static double filtrarCosteMateriaPrima() {
         
         //Variable temporal para guardar el valor recibido por el método
@@ -225,7 +268,16 @@ public class FabricaDeDulces3 {
         //Primer bucle para filtro de rango del coste de materia prima
         do {
             
-            //Inicializamos a true
+            //Inicializamos a true, esto se hace ya que en el primer ciclo del
+            //segundo bucle que contiene el try catch introducimos un valor
+            // numérico la variable seguir = false linea 256, luego si el número 
+            //no se comprende entre el rango predeterminado en el while del 
+            // primer bucle se repite la entrada de datos porque no se cumple la
+            //condicion, esta variable se quedaría a false , entonces al
+            //repetir el bucle se saltaria código ya que al ser la variable 
+            //seguir=false no entraría en el segundo bucle y habria errores en 
+            //el programa
+            
             seguir = true;
             
             //Segundo bucle que se repite si se introduce valores no numéricos
@@ -236,7 +288,8 @@ public class FabricaDeDulces3 {
                 //alfabéticos para transformarlos en números
                 
                 try {
-                    costeMateriaPrima = Double.parseDouble(leerCosteMateriaPrima());
+                    costeMateriaPrima =
+                            Double.parseDouble(leerCosteMateriaPrima());
                     
                     //Ponemos la booleana a false si el dato es correcto para
                     //salir del primer bucle
@@ -258,104 +311,233 @@ public class FabricaDeDulces3 {
                 JOptionPane.showMessageDialog(null,
                         "Precio incorrecto, repita");
             }
-
+            
+            
+            
+            //Condición para que se repita el bucle si el numero no se encuentra
+            //entre el rango
         } while (costeMateriaPrima < COSTE_MINIMO_MATERIA_PRIMA
                 || costeMateriaPrima > COSTE_MAXIMO_MATERIA_PRIMA);
-
+        
+        //Aqui devolvemos el valor de la materia prima ya filtrado y correcto
+        //Se guarda en una variable en el método mostrar programa
         return costeMateriaPrima;
     }
-
+    
+    
+    
+    
+    
+    //Este método te muestra entre que precio debe estar el coste de la mano de
+    //obra, devuelve el valor que escribas por teclado
+    //Este valor se guardara en una variable temporal en el método
+    //filtrarCosteManoObra
+    //Devuelve un valor String que será guardado posteriormente en variables del
+    //método filtrarCosteManoObra
     private static String leerCosteManoObra() {
 
         return JOptionPane.showInputDialog("Introduce el precio de la mano de obra"
                 + ", debe estar entre 0.5 y 0.9");
     }
-
+    
+    
+    
+    
+    
+    
+    
+    //Este metodo lo usamos para filtrar la entrada de datos y se encuentre
+    //entre los parámetros indicados
+    //Cun bucle que llama al método leerCosteManoObra cada vez que nos 
+    //equivoquemos al introducir los datos, mas explicado en el interior del
+    //método
+    //Devuelve un valor double que será guardado posteriormente en variables del
+    //método mostrarPrograma
     private static double filtrarCosteManoObra() {
-
+        
+        //Variable temporal para guardar el valor recibido por el método
+        //leerCosteManoObra
         double costeManoObra = 0;
+        
+        
+        //Constantes para marcar el máximo y el mínimo permitido de introducción
+        //de coste de mano de obra
         final double COSTE_MINIMO_MANO_OBRA = 0.5;
         final double COSTE_MAXIMO_MANO_OBRA = 0.9;
+        
+        
+        //Variable booleana para usar en el bucle de pedida de datos si no son
+        //correctos
         boolean seguir;
+        
+         //Primer bucle para filtro de rango del coste de materia prima
         do {
+            
+            //Inicializamos a true, esto se hace ya que en el primer ciclo del
+            //segundo bucle que contiene el try catch introducimos un valor
+            // numérico la variable seguir = false linea 353, luego si el número 
+            //no se comprende entre el rango predeterminado en el while del 
+            // primer bucle se repite la entrada de datos porque no se cumple la
+            //condicion, esta variable se quedaría a false , entonces al
+            //repetir el bucle se saltaria código ya que al ser la variable 
+            //seguir=false no entraría en el segundo bucle y habria errores en 
+            //el programa
+            
             seguir = true;
+            
+            //Segundo bucle que se repite si se introduce valores no numéricos
             do {
-
+                
+                //Try catch para controlar que al hacer la conversión de String
+                //a double no haya un error de introducción de carácteres
+                //alfabéticos para transformarlos en números
+                
                 try {
-                    costeManoObra = Double.parseDouble(leerCosteManoObra());
+                    costeManoObra =
+                            Double.parseDouble(leerCosteManoObra());
+                    
+                    //Ponemos la booleana a false si el dato es correcto para
+                    //salir del primer bucle
                     seguir = false;
                 } catch (NumberFormatException nfe) {
+                    
+                    //Mensaje que salta si hay error de introducción
                     JOptionPane.showMessageDialog(null,
                             "No has introducido un valor numerico");
                 }
 
             } while (seguir);
-
+            
+            //Condición que usamos para que salte un mensaje si el dato es 
+            //de valor numérico pero no se encuentra entre el rango
+            //predeterminado
             if (costeManoObra < COSTE_MINIMO_MANO_OBRA
                     || costeManoObra > COSTE_MAXIMO_MANO_OBRA) {
                 JOptionPane.showMessageDialog(null,
                         "Precio incorrecto, repita");
             }
-
+            
+            
+            
+            //Condición para que se repita el bucle si el numero no se encuentra
+            //entre el rango
         } while (costeManoObra < COSTE_MINIMO_MANO_OBRA
                 || costeManoObra > COSTE_MAXIMO_MANO_OBRA);
+        
+        //Aqui devolvemos el valor de la mano de obra ya filtrado y correcto
+        //Se guarda en una variable en el método mostrar programa
+        
 
         return costeManoObra;
 
     }
-
+    
+    
+    
+    
+    
+    
+    
+    //Este metodo recibe por parámetros el coste de la materia prima y el 
+    //coste de la mano de obra ya guardadas en variables en el método
+    //mostrarPrograma e internamente hace los cálculos para obtener el 
+    //coste de producción, tambíen se le pasa el código de producto, no es
+    //necesario , se usa para compleatar código
+    //Devuelve un valor double que será guardado posteriormente en variables del
+    //método mostrarPrograma
     private static double calcularCosteProduccion(double materiaPrima, double manoDeObra, String codigo) {
-
+        
+        //Variable temporal para calcular la suma de los costes
         double costeProduccion = materiaPrima + manoDeObra;
-
+        
+        //Mensaje que muestra el coste de producción de ese "codigo concreto"
         JOptionPane.showMessageDialog(null,
                 "El coste de producción del código " + codigo + " es: "
                 + String.format("%.2f", costeProduccion) + "€");
-
+        
+        //devolvemos el valor que se almacenara en una variable en el método 
+        //mostrarPrograma
         return costeProduccion;
 
     }
 
+    
+    
+    
+    //Este método recibe por parámetros el coste de producción del producto
+    //y el código de producto ya guardado en variables en el método 
+    //mostrarPrograma para calcular el coste de venta unitaria aquí el código de
+    //producto si es necesario ya que dependiendo de cuál sea el coste cambia
+    //Devuelve un valor double que será guardado posteriormente en variables del
+    //método mostrarPrograma
+    
     private static double calcularCosteVentaUnitaria(double costeProduccion, String codigo) {
-
+        
+        //Variable temporal para calcular el coste de venta unitaria
         double costeVentaUnitaria;
+        
+        //Variables constantes que tienen valor diferente según el código de
+        //producto
         final double PORCENTAJE_VENTA_UNITARIA_M1_M2_P1 = 0.5;
         final double PORCENTAJE_VENTA_RESTO_ARTICULOS = 0.65;
-
+        
+        
+        //Mediante este switch hacemos un pequeño filtrado para calcular el
+        //coste de venta unitaria dependiendo del codigo de producto
         switch (codigo) {
             case "M1","M2","P1":
-                //primer caso tercer switch
+                //Formula para calcular el coste de venta unitaria
                 costeVentaUnitaria = costeProduccion + costeProduccion
                         * PORCENTAJE_VENTA_UNITARIA_M1_M2_P1;
 
                 break;
             default:
-                //equivalente al segundo caso tercer switch
-                //ya qur ya filtramos la entrada de datos anteriormente
+                //equivalente a otro case
+                //ya que ya filtramos la entrada de datos anteriormente
+                
+                //Formula para calcular el coste de venta unitaria
                 costeVentaUnitaria = costeProduccion + costeProduccion
                         * PORCENTAJE_VENTA_RESTO_ARTICULOS;
         }
-
+        
+        
+        //Mensaje que muestra el precio de venta unitaria según el código
+        //Limitamos los decimales a 2
         JOptionPane.showMessageDialog(null,
                 "El coste de venta unitaria del código " + codigo + " es: "
                 + String.format("%.2f", costeVentaUnitaria) + "€");
-
+        
+        //Devolvemos el valor que será guardada en una variable en el método
+        //mostrarPrograma
         return costeVentaUnitaria;
 
     }
+    
+    
+    
+    //Este método recibe por parámetros el coste de producción, el coste de 
+    //venta unitaria y el precio de beneficio y devuelve un valor tipo entero
+    //Calcula el número de unidades necesarias para llegar al beneficio
+    //internamente con la fórmula
 
     private static int calcularUnidadesParaObtenerBeneficios(double costeProduccion, double costeVentaUnitaria, double precioBeneficio) {
-
+        
+        //Variable temporal para guardar el número de dulces
+        //Será guardada en una variable en el método mostrarPrograma
         double unidadesParaBeneficio;
-
+        
+        //Formula para calcular las unidades
         unidadesParaBeneficio = Math.ceil(precioBeneficio
                 / (costeVentaUnitaria - costeProduccion));
-
+        
+        
+        //Mensaje que muestra el numero de unidades necesarias
         JOptionPane.showMessageDialog(null,
                 "El numero de unidades para el beneficio son: "
                 + (int) unidadesParaBeneficio + " unidades");
-
+        //Hacemos conversiones explícitas de int a double para que nos
+        //devuelva valores enteros y no decimales
         return (int) unidadesParaBeneficio;
-
+        
     }
 }
