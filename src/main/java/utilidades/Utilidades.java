fe0b0ac73f083d70f10991775d5bc1462a24588f
JuanDiego <jmarmor2111@g.educaan.es>
2023-02-17 10:36:32 +0100

    examen casilla

--- src/main/java/utilidades/Utilidades.java
+++ src/main/java/utilidades/Utilidades.java
@@ -234,4 +234,18 @@ public class Utilidades {
             System.out.println("");
         }
     }
+    public static void imprimirMatrizChar(char[][]mat){
+    
+    for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (j < mat[i].length - 1) {
+                    System.out.print("[" + mat[i][j] + "]" + "-");
+                } else {
+                    System.out.print("[" + mat[i][j] + "]");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
 }
